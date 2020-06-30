664fa56659e6284d331f2101a802a0a90f7106d9
Tofu404 <1144290250@qq.com>
2020-06-30 19:35:57 +0800

    push to git

---
+++ MyView/app/src/main/java/create/by/myview/views/MyButton.java
@@ -0,0 +1,40 @@
+package create.by.myview.views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import create.by.myview.R;
+
+public class MyButton extends androidx.appcompat.widget.AppCompatButton {
+
+    private int mMyButtonBgColor;
+    private int mMyButtonRadius;
+
+
+
+    public MyButton(Context context) {
+        this(context,null);
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        @SuppressLint("Recycle")
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MyButton, defStyleAttr, 0);
+        mMyButtonBgColor = a.getColor(R.styleable.MyButton_bg_color, Color.GRAY);
+        mMyButtonRadius = a.getInt(R.styleable.MyButton_radius, 0);
+        init();
+    }
+
+    private void init() {
+        
+    }
+
+
+}
--- ShanHuReport/MyDemo/releasePackingShell.bat
+++ ShanHuReport/MyDemo/releasePackingShell.bat
@@ -2,7 +2,7 @@
 copy /y D:\Android_Java\ShanHuReport\config\1.dat D:\Android_Java\ShanHuReport\MyDemo\app\src\main\assets\40805.dat
 call gradlew app:assembleMyApp1
 adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp1\debug\app-myApp1-debug.apk
-adb shell am start -n nihao.com/create.by.tianze.MainActivity
+adb shell am start -n com.hh.beikemm/create.by.tianze.MainActivity
 :loop1
 echo.
 echo.
@@ -20,11 +20,127 @@ echo.
 echo.
 set /p num=
 if %num% == 1 (
-	adb uninstall nihao.com
+	adb uninstall com.hh.beikemm
 	adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp1\debug\app-myApp1-debug.apk
-	adb shell am start -n nihao.com/create.by.tianze.MainActivity
+	adb shell am start -n com.hh.beikemm/create.by.tianze.MainActivity
 	set /a num=0
 	goto loop1
 )
-adb uninstall nihao.com
+adb uninstall com.hh.beikemm
+
+copy /y D:\Android_Java\ShanHuReport\config\2.dat D:\Android_Java\ShanHuReport\MyDemo\app\src\main\assets\40805.dat
+call gradlew app:assembleMyApp2
+adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp2\debug\app-myApp2-debug.apk
+adb shell am start -n com.boshide.kingbeans/create.by.tianze.MainActivity
+:loop2
+echo.
+echo.
+echo ################################
+echo.
+echo.
+echo input "1" to reinstall the app
+echo.
+echo.
+echo input "2" to go on
+echo.
+echo.
+echo ################################
+echo.
+echo.
+set /p num=
+if %num% == 1 (
+	adb uninstall com.boshide.kingbeans
+	adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp2\debug\app-myApp2-debug.apk
+	adb shell am start -n com.boshide.kingbeans/create.by.tianze.MainActivity
+	set /a num=0
+	goto loop2
+)
+adb uninstall com.boshide.kingbeans
+
+copy /y D:\Android_Java\ShanHuReport\config\3.dat D:\Android_Java\ShanHuReport\MyDemo\app\src\main\assets\40805.dat
+call gradlew app:assembleMyApp3
+adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp3\debug\app-myApp3-debug.apk
+adb shell am start -n com.bcxgame.lw.dzfhd/create.by.tianze.MainActivity
+:loop3
+echo.
+echo.
+echo ################################
+echo.
+echo.
+echo input "1" to reinstall the app
+echo.
+echo.
+echo input "2" to go on
+echo.
+echo.
+echo ################################
+echo.
+echo.
+set /p num=
+if %num% == 1 (
+	adb uninstall com.bcxgame.lw.dzfhd
+	adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp3\debug\app-myApp3-debug.apk
+	adb shell am start -n com.bcxgame.lw.dzfhd/create.by.tianze.MainActivity
+	set /a num=0
+	goto loop3
+)
+adb uninstall com.bcxgame.lw.dzfhd
+
+copy /y D:\Android_Java\ShanHuReport\config\4.dat D:\Android_Java\ShanHuReport\MyDemo\app\src\main\assets\40805.dat
+call gradlew app:assembleMyApp4
+adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp4\debug\app-myApp4-debug.apk
+adb shell am start -n com.tyrell.game.zt.cygs2/create.by.tianze.MainActivity
+:loop4
+echo.
+echo.
+echo ################################
+echo.
+echo.
+echo input "1" to reinstall the app
+echo.
+echo.
+echo input "2" to go on
+echo.
+echo.
+echo ################################
+echo.
+echo.
+set /p num=
+if %num% == 1 (
+	adb uninstall com.tyrell.game.zt.cygs2
+	adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp4\debug\app-myApp4-debug.apk
+	adb shell am start -n com.tyrell.game.zt.cygs2/create.by.tianze.MainActivity
+	set /a num=0
+	goto loop4
+)
+adb uninstall com.tyrell.game.zt.cygs2
+
+copy /y D:\Android_Java\ShanHuReport\config\5.dat D:\Android_Java\ShanHuReport\MyDemo\app\src\main\assets\40805.dat
+call gradlew app:assembleMyApp5
+adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp5\debug\app-myApp5-debug.apk
+adb shell am start -n com.walk.moneycome.bodyhealth/create.by.tianze.MainActivity
+:loop5
+echo.
+echo.
+echo ################################
+echo.
+echo.
+echo input "1" to reinstall the app
+echo.
+echo.
+echo input "2" to go on
+echo.
+echo.
+echo ################################
+echo.
+echo.
+set /p num=
+if %num% == 1 (
+	adb uninstall com.walk.moneycome.bodyhealth
+	adb install -r D:\Android_Java\ShanHuReport\MyDemo\app\build\outputs\apk\myApp5\debug\app-myApp5-debug.apk
+	adb shell am start -n com.walk.moneycome.bodyhealth/create.by.tianze.MainActivity
+	set /a num=0
+	goto loop5
+)
+adb uninstall com.walk.moneycome.bodyhealth
 
