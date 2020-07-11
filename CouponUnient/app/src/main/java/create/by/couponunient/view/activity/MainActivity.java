29cbf5d305e0b314a59cef2ab4b96a1dee9cb5c9
Tofu404 <1144290250@qq.com>
2020-07-11 20:07:58 +0800

    push to git

---
+++ CouponUnient/app/src/main/java/create/by/couponunient/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package create.by.couponunient.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import create.by.couponunient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
