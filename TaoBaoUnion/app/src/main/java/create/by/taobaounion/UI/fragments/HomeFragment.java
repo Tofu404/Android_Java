a33e1c71704302e9d06ba1906860b9f671de2276
Tofu404 <1144290250@qq.com>
2020-07-13 21:36:56 +0800

    push to git

---
+++ TaoBaoUnion/app/src/main/java/create/by/taobaounion/UI/fragments/HomeFragment.java
@@ -0,0 +1,12 @@
+package create.by.taobaounion.UI.fragments;
+
+import create.by.taobaounion.R;
+import create.by.taobaounion.UI.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    protected int setResourceId() {
+        return R.layout.fragment_home;
+    }
+}
